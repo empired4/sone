bac0c4837a5c746dd75457a55dd92508134d20df
empired4 <empired464@gmail.com>
2025-09-21 17:13:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ class Config(object):
     API_ID = 26741932
     API_HASH = "d9037cbe01f292eb440468c39c3dc41d"
     ADMIN_ID = [7047543426]
+    
     DB_URL = "mongodb+srv://navedmohammad2516:zu02cmOW6medghcJ@cluster0.cq1x5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    DB_NAME = os.environ.get("DB_NAME")
+    DB_NAME = os.environ.get("DB_NAME", "wadi")   # ✅ fix applied
+    
     TXT_LOG = -1002322908140
     HOST = "https://www.masterapi.tech"
-
